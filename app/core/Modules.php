5aed8fff397b8a42db9f9cbbb2b919e6d0f76d37
Alan Cole <alan@wax.im>
2014-12-17 18:38:54 +0000

    Begining modules, this may take a while

---
+++ app/core/Modules.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Transaqt;
+
+class Modules {
+
+    /*
+      Our container for all things module.
+
+      Notes:
+      This basic idea is that modules will
+      build to a giant object of module
+      information and include all its files
+      at which point the module should be
+      called using its namespace
+
+        \ModuleNamespace\Controller::function();
+        \ModuleNamespace\Model::function();
+        \ModuleNamespace\View::function();
+
+      The module system will take of adding controllers
+      models, views, assets, events, installs, uninstalls
+      and all modules will have access to the Transaqt::Core()
+
+
+    */
+    protected static $modules = array();
+
+    /*
+      collect modules
+    */
+
+    /*
+      enable module
+    */
+
+    /*
+      disable module
+    */
+
+    /*
+      install module
+    */
+
+    /*
+      uninstall module
+    */
+
+}
